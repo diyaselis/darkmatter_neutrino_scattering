98d3a07ad8b4178c9a6b508b82205f982f7709a8
diyaselis <diyaselis@gmail.com>
2023-01-02 13:34:53 -0500

    updates

--- code/surv_probability.py
+++ code/surv_probability.py
@@ -3,28 +3,28 @@ import numpy as np
 import seaborn as sns
 import matplotlib
 import matplotlib.pyplot as plt
-from matplotlib import ticker, cm
+from matplotlib import ticker, cm, colors
 matplotlib.style.use('../paper.mplstyle')
 
 cl90 = 2.71
 cl95 = 3.84
 cl99= 6.63
 
-n = 2
+n = 3
 interaction = 'scalar'
 g_list = np.logspace(-3,0,n) # eV
 mphi_list = np.logspace(5,10,n) # eV
 mx_list = np.logspace(5,12,n) # eV
 
-def no_neg(array):
-    m,n = array.shape
-    for i in range(m):
-        for j in range(n):
-            if array[i][j] < 0:
-                array[i][j] = 0
-    return array
+# def no_neg(array):
+#     m,n = array.shape
+#     for i in range(m):
+#         for j in range(n):
+#             if array[i][j] < 0:
+#                 array[i][j] = 0
+#     return array
 
-ts_g_null_list = np.array([[BinnedLikelihoodFunction(3e-1,mphi,mx,interaction)()[0] for mx in mx_list] for mphi in mphi_list])
+# ts_g_null_list = np.array([[BinnedLikelihoodFunction(3e-1,mphi,mx,interaction)()[0] for mx in mx_list] for mphi in mphi_list])
 # ts_mphi_null_list = np.array([[BinnedLikelihoodFunction(g,1e7,mx,interaction)()[0] for mx in mx_list] for g in g_list])
 # ts_mx_null_list = np.array([[BinnedLikelihoodFunction(g,mphi,1e8,interaction)()[0] for mphi in mphi_list] for g in g_list])
 # ts_g_null_list = no_neg(ts_g_null_list)
@@ -39,29 +39,40 @@ ts_g_null_list = np.array([[BinnedLikelihoodFunction(3e-1,mphi,mx,interaction)()
 # ts_mx_DM_list = no_neg(ts_mx_DM_list)
 
 
-norm = lambda z: matplotlib.colors.LogNorm(vmin=z.min(), vmax=z.max())
+norm = lambda z: colors.LogNorm(vmin=z.min(), vmax=z.max())
 
 X, Y = np.meshgrid(mphi_list,mx_list)
 
 Z = np.array([[BinnedLikelihoodFunction(3e-1,X[i][j],Y[i][j],interaction)()[0] for i in range(X.shape[0])] for j in range(X.shape[1])])
 Z = np.ma.masked_where(Z <= 0, Z)
+lognorm=norm(Z)
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(8,6))
 # lev_exp = np.arange(np.floor(np.log10(Z.min())-1),np.ceil(np.log10(Z.max())+1))
 # levs = np.power(10, lev_exp)
 # norm=matplotlib.colors.LogNorm()
 # locator = ticker.LogLocator()
 # extent1 = mphi_list.min(), mphi_list.max(), mx_list.min(), mx_list.max()
 # plt.imshow(Z, extent=extent1, norm=norm(Z))
-# cp = ax.contourf(X,Y,Z, cmap="bone") #locator = ticker.LogLocator()
-cp = ax.contourf(X,Y,Z, locator = ticker.LogLocator(),  cmap="bone")
-plt.colorbar(cp)
+# n_levels = 10
+# levels = np.logspace(np.log10(Z.min()),np.log10(Z.max()), n_levels)
+
+locator = ticker.LogLocator(base=10)
+# cp = ax.pcolormesh(X,Y,Z, cmap="bone",norm=norm(Z),shading='nearest') #locator = ticker.LogLocator()
+cp = ax.contourf(X,Y,Z, locator=locator, cmap="bone") #locator = ticker.LogLocator()
+
+cbar  = plt.colorbar(cp, ticks=locator) #, extend='max'
+cbar.set_label(r'$-2\Delta LLH$',rotation=90)
+# cbar.minorticks_off()
+# cbar.ax.set_yscale('log')
 plt.loglog()
 plt.xlabel(r'$m_\phi$ (GeV)')
 plt.ylabel(r'$m_\chi$ (GeV)')
+plt.xlim(X.min(), X.max())
+plt.ylim(Y.min(), Y.max())
 plt.show()
-# plt.savefig('plots/TS_2D_g.png',dpi=200)
-# plt.close()
+plt.savefig('plots/TS_2D_g.png',dpi=200)
+plt.close()
 quit()
 
 
